768b79dbacfa5283a2ee23bd8db0b509f3781763
Agustin Bettig <agustinbettigyt@gmail.com>
2023-07-08 20:09:10 -0300

    casiready Agu

--- assets/js/wallet.js
+++ assets/js/wallet.js
@@ -1,14 +1,41 @@
 $(document).ready(function() {
-  $(".card").click(function() {
-    // Si la tarjeta ya está activa, la desactiva
-    if ($(this).hasClass("active")) {
-      $(this).removeClass("active");
+  $('.card').click(function() {
+    if ($(this).hasClass('hidden')) {
+      // Restaurar estado original
+      $('.card').css({
+        'transform': '',
+        'z-index': ''
+      });
+      $('.card').removeClass('hidden');
+      $('.card').fadeIn();
+      $('.card-info').fadeOut(300, function() {
+        $(this).removeClass('fade-in').addClass('hidden');
+      });
     } else {
-      // Remover la clase 'active' de todas las tarjetas
-      $(".card").removeClass("active");
+      // Obtener el índice de la tarjeta clickeada
+      var index = $(this).index();
 
-      // Agregar la clase 'active' a la tarjeta seleccionada
-      $(this).addClass("active");
+      if ($(this).css('z-index') === '4') {
+        // Restaurar estado original
+        $(this).css({
+          'transform': '',
+          'z-index': ''
+        });
+        $('.card').fadeIn();
+        $('.card-info').fadeOut(300, function() {
+          $(this).addClass('hidden');
+        });
+      } else {
+        // Aplicar el transform: translateY(0px) y z-index: 4 a la tarjeta clickeada
+        $(this).css({
+          'transform': 'translateY(0px)',
+          'z-index': 4
+        });
+
+        // Ocultar las otras tarjetas con animación fadeOut
+        $('.card').not(this).fadeOut();
+        $('.card-info').removeClass('hidden').addClass('fade-in').fadeIn(300);
+      }
     }
   });
 });
